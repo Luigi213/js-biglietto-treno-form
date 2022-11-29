d4c856078fc0f1e53bda76581f12279a03fba190
Luigi213 <reyesgianluigi88@gmail.com>
2022-11-29 09:33:00 +0100

    optimize

--- js/script.js
+++ js/script.js
@@ -3,18 +3,16 @@ button.addEventListener('click',function(){
     // 1,2- inserire i chilometri e l'eta
     let kilometres = document.getElementById("km").value;
     let age = document.getElementById("age").value;
-    let underage = 'underage';
-    let over = 'over-65';
     // 3- calcolo del prezzo del biglietto con le dovute le regole:
         // 3.1 applicare il prezzo in base ai chilometri da percorrere (0.21 € al km)
         let price_ticket;
         // 3.2- applicare lo sconto in base all'età
-        if (age == underage){ 
+        if (age == 'underage'){ 
             // minorenni 20%
             price_ticket = (0.21 * kilometres * 0.8).toFixed(2);
             console.log(price_ticket)
         }
-        else if (age == over){
+        else if (age == 'over-65'){
             // over 65 di 40%
             price_ticket = (0.21 * kilometres * 0.6).toFixed(2);
         }
